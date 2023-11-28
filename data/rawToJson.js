88641c6a36eb6c7ae805d9bb8d4833866b9e2e49
zuhaeerislam10 <zuhaeerislam@brandeis.edu>
2023-11-28 10:08:08 -0500

    fixed issue with homeless data

--- data/rawToJson.js
+++ data/rawToJson.js
@@ -18,13 +18,6 @@ fs.createReadStream("data/raw-merged-data.csv")
 
     fs.writeFile("data/data.json", jsonData, (err) => {
       if (err) throw err;
-      console.log("Data converted and saved to output.json");
+      console.log("Data converted and saved to data.json");
     });
   });
-
-// check if csv file correctly parsed
-fs.readFile("data.json", "utf8", (err, data) => {
-  const jsonData = JSON.parse(data);
-  const numberOfObjects = jsonData.length;
-  console.log(`Number of objects in the JSON file: ${numberOfObjects}`);
-});
